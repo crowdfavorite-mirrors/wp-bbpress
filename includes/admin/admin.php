a22f4e835e0ab9d2036416c677f1fc904060af71
Chris Mospaw <cmospaw@crowdfavorite.com>
2012-11-20 15:21:25 -0700

    avatar upload JS error fix

--- includes/admin/admin.php
+++ includes/admin/admin.php
@@ -540,12 +540,15 @@ class BBP_Admin {
 
 				var bbp_topic_id = jQuery( '#bbp_topic_id' );
 
-				bbp_topic_id.suggest( ajaxurl + '?action=bbp_suggest_topic', {
-					onSelect: function() {
-						var value = this.value;
-						bbp_topic_id.val( value.substr( 0, value.indexOf( ' ' ) ) );
-					}
-				} );
+				// Fix to avoid JS errors when cropping new avatar
+				if (typeof bbp_topic_id.suggest !== "undefined") {}
+					bbp_topic_id.suggest( ajaxurl + '?action=bbp_suggest_topic', {
+						onSelect: function() {
+							var value = this.value;
+							bbp_topic_id.val( value.substr( 0, value.indexOf( ' ' ) ) );
+						}
+					} );
+				}
 			});
 		</script>
 
